e064a01c12d824b03b1b9f0448c686eb8c409fc8
nikitacarelov <51008741+nikitacarelov@users.noreply.github.com>
2022-01-29 11:08:12 -0500

    woo

---
+++ main.py
@@ -0,0 +1,9 @@
+def main_wrapper():
+
+
+    print(f"This is the start of our Python Project. This function's name is {main_wrapper.__name__}")
+    #Code
+
+    print("This is the end of our Python project.")
+if __name__ == "__main__":
+    main_wrapper()
